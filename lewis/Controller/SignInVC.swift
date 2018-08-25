8cfde972c9a7c47aec2113dd9ebbd3f03ea0cdbe
vriznet <vriz@vriz.net>
2018-08-25 12:45:22 +0900

    Completed setting up Firebase integration

---
+++ lewis/Controller/SignInVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  lewis
+//
+//  Created by vriz on 2018. 8. 25..
+//  Copyright © 2018년 vriz. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
